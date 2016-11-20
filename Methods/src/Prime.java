f83195b09df7bf73f34f05d02b270cfc8de8bcd7
fahim2020 <omfahi@gmail.com>
2016-11-21 00:37:14 +0100

    Update Prime.java

--- Methods/src/Prime.java
+++ Methods/src/Prime.java
@@ -8,31 +8,29 @@ public class prime {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please Enter a Number : ");
 		int n = scan.nextInt();
-		while (j <= n / 2) // start loop, during loop j will become each number
-							// between 2 and
-		{ // the entered number divided by 2
-			if (n % j == 0) // If their is no remainder from your number divided
-							// by j...
+		while (j <= n / 2) // start loop 
+		{ 
+			if (n % j == 0) 
 			{
-				 result = 1; // Then result is set to 1 as the number divides
-							// equally by another number, hergo
+				 result = 1; 
 			} // it is not a prime number
-			j++; // Increment j to the next number to test against the number
-					// you entered
+			j++; 
 		}
 		if (result == 1) // check the result from the loop
 		{
-			System.out.println("Number: " + n + " is Not Prime."); // If result
-																	// 1 then a
-																	// prime
+			System.out.println("Number: " + n + " is Not Prime."); 
+			
+																	
 		} else {
-			System.out.println("Number: " + n + " is Prime. "); // If result is
-																// not 1 it's
-																// not a prime
+			System.out.println("Number: " + n + " is Prime. "); 
+																
 		}
 
 		// todo: write a method which tells you if a given number is a prime or
 		// not
+		//A prime number (or a prime) is a natural number greater than 1 that has 
+		//no positive divisors other than 1 and itself. A natural number greater than
+		//1 that is not a prime number is called a composite number.
 
 	}
 
