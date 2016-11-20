5d3e46742a311faf2a39e8654df6ac2fab1f0962
fahim2020 <omfahi@gmail.com>
2016-11-21 00:10:38 +0100

    Update Circle.java

--- Methods/src/Circle.java
+++ Methods/src/Circle.java
@@ -1,5 +1,22 @@
 
-public class Circle {
-	//todo: add some methods to compute a circle's diameter, circumference and area.
-	//print some outputs to confirm you methods are working right.
+import java.util.Scanner;
+
+public class circle {
+	// todo: add some methods to compute a circle's diameter, circumference and
+	// area.
+	// print some outputs to confirm you methods are working right.
+	public static final double PI = 3.14;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter raduis: ");
+		double raduis = scan.nextDouble();
+		double area = PI * raduis * raduis;
+		double dia= 2* raduis;
+		double circum=2*PI*raduis;
+		System.out.println("Circle area = " + area);
+		System.out.println("Circle diameter = " + dia);
+		System.out.println("Circle Circumference = " + circum);
+			
+	}
 }
