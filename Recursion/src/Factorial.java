f12b29fc732e25152f69c1d32baa1f188c7be9cc
fahim2020 <omfahi@gmail.com>
2016-11-21 15:20:52 +0100

    Update Factorial.java

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,4 +1,20 @@
-
+import java.util.Scanner;
 public class Factorial {
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	public static void main(String[] args){
+		Scanner scan =new Scanner(System.in);
+		System.out.println("enter a number:");
+		int n =scan.nextInt();
+		 
+	        int fact = 1; // this  will be the result
+	        for (int i = 1; i <= n; i++) {
+	            fact *= i;
+	     
+	       
+	    }
+	        System.out.println("factorial is :"+fact);
+		
+	}
+	
+	
 }
