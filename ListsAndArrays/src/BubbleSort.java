7c39c81060a0814646c1e3b972d5170ae4a151c3
fahim2020 <omfahi@gmail.com>
2016-12-05 00:00:48 +0100

    Update BubbleSort.java

--- ListsAndArrays/src/BubbleSort.java
+++ ListsAndArrays/src/BubbleSort.java
@@ -1,5 +1,28 @@
 
 public class BubbleSort {
+	
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] Aray = {10,5,9,56,1};
+		for(int i=0; i < Aray.length;i++)
+		{
+			for(int j=0; j < Aray.length;j++)
+			{
+				if(Aray[i]<Aray[j])
+				{
+					int temp=Aray[i];
+					Aray[i]=Aray[j];
+					Aray[j]=temp;
+				}
+			}
+		}
+		for(int i=0; i < Aray.length;i++)
+		{
+			System.out.println(Aray[i]+" ");
+		}
+	}
+
+	}
 //intermediate
 	
 	//todo: sort an array using bubblesort.
